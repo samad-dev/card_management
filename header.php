bade21a5f5c6ff50f2a0a745c41d6c8b698a5bd8
owais gul <owaiskk366@gmail.com>
2025-03-04 15:15:55 +0500

    modified header.php: excessive memory problem

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <?php 
-include("../header.php"); 
+// include("../header.php"); 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type");
